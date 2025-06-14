694572c740691f6ad566486e026fcc3862d269f6
shaurya super <shauryabansal2005@gmail.com>
2025-06-15 01:10:52 +0530

    done some changes and added proctile

--- app.py
+++ app.py
@@ -131,5 +131,6 @@ def predict():
             'message': str(e)
         }), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
